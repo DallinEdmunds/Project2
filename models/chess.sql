2190bba33561f562201b49a017eeb3917fb18e4b
Dallin Edmunds <51213681+DallinEdmunds@users.noreply.github.com>
2019-10-16 21:05:55 -0600

    created mySQL server for project. Created requirements for the project to work.

---
+++ models/chess.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE chess_db;
+
+USE chess_db;
+
+CREATE TABLE Users (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255)
+);
---
+++ notes.js
@@ -0,0 +1,13 @@
+// 1. chess board(squares on the board (64))
+// 2. two players
+// 3. 16 pieces for each side (8 pawns, 2 rooks, 2 knights, 2 bishops, king, queen)
+// 4. define how each piece moves and the placement of the pieces
+// 5. define a kill
+// 6. define a win/lose
+
+// what to incorporate in the front/backend
+// 1. user log in
+// 2. remembering where user left off at. 
+// 3. incorporate selecting pieces, seeing move options, and selecting location.
+// 4. integrating snake button to turn chess board into snake game. 
+// 5. see if it’s possible to play chess in node terminal.
